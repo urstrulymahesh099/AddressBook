07716eeca68f41f929694f77d2bda3a88cb02612
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-10 05:33:35 +0530

    [Mahesh] Add:Contact Details

---
+++ AddressBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using AddressBook;
+using System;
+using System.Net;
+
+namespace AddresBook 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact();
+            {
+                contact.FirstName = Console.ReadLine();
+                contact.LastName = Console.ReadLine();
+                contact.Address = Console.ReadLine();
+                contact.City = Console.ReadLine();
+                contact.State = Console.ReadLine();
+                contact.Zip = Convert.ToInt64(Console.ReadLine());
+                contact.PhoneNumber = Console.ReadLine();
+                contact.Email = Console.ReadLine();
+            };
+            Console.WriteLine("Contact details" + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+        }
+    }
+}
