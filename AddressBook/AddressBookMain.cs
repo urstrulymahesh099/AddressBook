d482f2ad4e09c73c0f7d6a60ae5bf944341e5ca4
Mahesh H <114014134+urstrulymahesh099@users.noreply.github.com>
2022-11-11 05:31:14 +0530

    Update AddressBookMain.cs

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -13,14 +13,14 @@ namespace AddressBook
 
             Contact contact = new Contact()
             {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToInt64(Console.ReadLine()),
-                PhoneNumber = Console.ReadLine(),
-                Email = Console.ReadLine()
+                FirstName = Console.ReadLine();
+                LastName = Console.ReadLine();
+                Address = Console.ReadLine();
+                City = Console.ReadLine();
+                State = Console.ReadLine();
+                Zip = Convert.ToInt64(Console.ReadLine());
+                PhoneNumber = Console.ReadLine();
+                Email = Console.ReadLine();
 
             };
             Console.WriteLine("Contact Details:" + "\n" + "FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNumber + "\n" + "Email: " + contact.Email);
